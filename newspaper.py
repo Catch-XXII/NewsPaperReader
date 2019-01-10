5850b36b3d7873a298dc8a268424fede9b867cc3
Catch-XXII <cgultekink@gmail.com>
2019-01-10 16:55:36 +0300

    Create newspaper.py
    don't forget to change the filename = '' path accordingly.

---
+++ newspaper.py
@@ -0,0 +1,23 @@
+import webbrowser
+import codecs
+filename = '.../news.txt'
+path_to_browser = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+url = list()
+name = list()
+
+with codecs.open(filename, 'r', encoding='utf8') as f:
+    next(f)
+    print('{:>15s}{:>15s}{:>15s}{:>30s}'.format('Name', 'Frequency', 'Circulation', 'Web-site'))
+    for row in f:
+        rows = row.split()
+        print('{:>15s}{:>15s}{:>15s}{:>30s}'.format(rows[0], rows[1], rows[2], rows[3]))
+        url.append(rows[3])
+        name.append(rows[0])
+
+selection = input('\nLütfen bir gazete seçiniz: ').capitalize()
+
+for i in range(len(name)):
+    if selection == name[i]:
+        print('Seçtiğiniz gazete: ', name[i])
+        webbrowser.get(path_to_browser).open(url[i])
+
